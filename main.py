49da84ddd4f3e1ca385e81731e6f6654322d7cb5
chajie0709 <xuejeffer@gmail.com>
2024-10-24 11:46:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -255,10 +255,7 @@ if __name__ == "__main__":
             _check_item = datas.get("MIMOTION", [])[i]
             #print(_check_item)
             msg += MiMotion(check_item=_check_item).main()
-            msg += os.environ["CONFIG"]
         print(msg)
-        print('1111')
-        print(os.environ["CONFIG"])
         # 酷推skey和server酱sckey和企业微信设置，只用填一个其它留空即可
         if datas.get("SKEY"):
             skey = datas.get("SKEY")
